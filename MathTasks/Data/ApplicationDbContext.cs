e3a36e2122df8eee2f9f5f0e575781b1ca431cea
Dima Osipow <dimaosipow0@gmail.com>
2021-09-25 12:59:25 +0300

    created a module and tables in the database for tasks

--- MathTasks/Data/ApplicationDbContext.cs
+++ MathTasks/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using MathTasks.Data.Entityes;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,7 @@ namespace MathTasks.Data
             : base(options)
         {
         }
+
+        public DbSet <Tasks> Tasks { get; set; }
     }
 }
---
+++ MathTasks/SampleData.cs
@@ -0,0 +1,21 @@
+﻿using MathTasks.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MathTasks
+{
+    public static class SampleData
+    {
+        public static void initData (ApplicationDbContext context)
+        {
+            if (!context.Tasks.Any())
+            {
+                context.Tasks.Add(new Data.Entityes.Tasks() { title = "aua", Html = "asa " }); ;
+                context.SaveChanges();
+            }
+            
+        }
+    }
+}
